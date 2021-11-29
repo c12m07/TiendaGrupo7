0a5a03ae580ce2c0f8c3edf4a5fc23e7f87e688f
CamiloVillada <91029632+CamiloVillada@users.noreply.github.com>
2021-11-29 09:39:35 -0500

    Add files via upload

--- Tienda/Front-end/app.js
+++ Tienda/Front-end/app.js
@@ -2,64 +2,92 @@ const express = require('express');
 const path = require('path');
 const bodyParser = require('body-parser')
 const app = express();
-const bcrypt= require('bcrypt');
-const mongoose=require('mongoose');
-const user = require('./public/user.js');
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended:false}));
 app.use(express.static(path.join(__dirname,'public')));
-const mongo_uri ='mongodb://localhost:27017/tienda'
-mongoose.connect(mongo_uri,function(err){
-    if(err){
-        console.log('Inicia Log de errores de Mongo');
-        throw  err;
-        console.log('Finaliza Log de errores de Mongo');
-    }
-    else{
-        console.log('se conectó a ${mongo_uri}');
-    }
+const http = require("http");
+
+function ingreso(){
+
+  var u="admininicial";
+  var c="admin123456";
+
+
+  if(document.form.login.value==u && document.form.password.value==c){
+  alert ("Bienvenido a Tienda Generica");
+  window.location="menu.html";}
+  else{
+  alert ("Usuario o Contraseña Incorrectos");
+
+}
+}
+
+app.get('/find',(req,resp) => { 
+    http
+    .get("http://localhost:8080/api/tutorials", (resp) => {
+      let data = "";
+      // A chunk of data has been recieved. Append it with the previously retrieved chunk of data
+      resp.on("data", (chunk) => {
+        data += chunk;
+      });
+  
+      // when the whole response is received, parse the result and Print it in the console
+      resp.on("end", () => {
+        console.log(JSON.parse(data));
+      });
+    })
+    .on("error", (err) => {
+      console.log("Error: " + err.message);
+    });
+  });
+
+ 
+  app.post('/crear',(req,res) => { 
+      console.log('body: ',req.body);
+    const {title,description} =req.body;
+    const data = JSON.stringify({
+        description: description,
+        title: title,
+      });
+    
+    const options = {
+        host: "localhost",
+        port: 8080,
+        path: "/api/tutorials",
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+          "Content-Length": data.length,
+        },
+      };
+      
+      
+      
+      req = http.request(options, (res) => {
+            //status code of the request sent
+            console.log("statusCode: ", res.statusCode);
+            let result = "";
+            // A chunk of data has been recieved. Append it with the previously retrieved chunk of data
+            res.on("data", (chunk) => {
+              result += chunk;
+            });
+            //The whole response has been received. Display it into the console.
+            res.on("end", () => {
+              console.log("Result is: " + result);
+            });
+          });
+          //error if any problem with the request
+          req.on("error", (err) => {
+            console.log("Error: " + err.message);
+          });
+          //write data to request body
+          req.write(data);
+          //to signify the end of the request - even if there is no data being written to the request body.
+          req.end();
 });
 
-app.post('/register',(req,res) => { 
-    
-    const {username,password} =req.body;
-    console.log('Entra a metodo post/register');
-const  User=new user({username,password});
-console.log('Declara variable user');
-    User.save(err =>{
-        if(err){
-            res.status(500).send('Error al registrar usuario');
-        }
-        else{
-            res.status(200).send('Usuario Registrado');
-            return res.redirect('http://localhost:3000/Principal.html');0.
-        }
-    });
-    
-});
-app.post('/authenticate',(req,res) => { 
-    const {username,password} =req.body;
-    user.findOne({username},(err,user) => {
-        if(err){
-            res.status(500).send('Error al autenticar usuario');
-        }else if(!user){
-            res.status(500).send('El usuario no existe');
-        } else
-        {
-            user.isCorrectPassword(password,(err,result)=>{
-                if(err){
-                    res.status(500).send('Error al autenticar');
-                }else if(result){
-                    // res.status(200).send('Usuario logueado correctamente');
-                    return res.redirect('http://localhost:3000/Principal.html');
-                }else{
-                    res.status(500).send('usuario o clave incorrecta');
-                }
-            });
-        }
-    });
-});
-app.listen(3000,() => {
-        console.log('Servidor Inicia puerto 3000');
-})
+
+
+app.listen(3000,() => console.log('Servidor Inicia puerto 3000'));
+
 module.exports=app;
